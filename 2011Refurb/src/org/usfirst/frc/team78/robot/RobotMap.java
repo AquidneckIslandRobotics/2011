c986e3e604a5097f3c3126b817296d66db5e92f6
alex <alexandercorey662@gmail.com>
2017-11-15 18:24:52 -0500

    First Commit
    initial 2011 commit

---
+++ 2011Refurb/src/org/usfirst/frc/team78/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team78.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static int RIGHT_FRONT = 0;
+	public static int RIGHT_BACK = 1;
+	public static int LEFT_FRONT = 2;
+	public static int LEFT_BACK = 3;
+	
+}
---
+++ 2011Refurb/src/org/usfirst/frc/team78/robot/subsystems/Chassis.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team78.robot.subsystems;
+
+import org.usfirst.frc.team78.robot.Robot;
+import org.usfirst.frc.team78.robot.RobotMap;
+
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+
+
+public class Chassis extends Subsystem {
+
+	public CANTalon rightFront = new CANTalon(RobotMap.RIGHT_FRONT);
+	public CANTalon rightBack = new CANTalon(RobotMap.RIGHT_BACK);
+	public CANTalon leftFront = new CANTalon(RobotMap.LEFT_FRONT);
+	public CANTalon leftBack = new CANTalon(RobotMap.LEFT_BACK);
+	
+	public void motorInit(){
+		rightBack.changeControlMode(TalonControlMode.Follower);
+		rightBack.set(rightFront.getDeviceID());
+		rightFront.reverseOutput(true);
+		leftBack.changeControlMode(TalonControlMode.Follower);
+		leftBack.set(leftFront.getDeviceID());
+		leftFront.reverseOutput(false);
+		
+	}
+
+	public void driveWithJoysticks(){
+		
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
